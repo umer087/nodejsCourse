9aa0d657c0cde15b914a63fce9cfa972d98aaa38
umer087 <hanifumar087@gmail.com>
2022-07-01 14:02:02 +0500

    section 4 17 yarg commands

--- app.js
+++ app.js
@@ -1,4 +1,49 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js')
+const yargs = require('yargs');
+const { string } = require('yargs');
 
-console.log(chalk.blue(getNotes()));
+yargs.command({
+    command: 'add',
+    describe: 'Adding a command',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv){
+        console.log(`Add handler\n Title: ${argv.title} \n Body: ${argv.body}`);
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing a note');
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List the note',
+    handler: function(){
+        console.log('List of notes');
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read the note',
+    handler: function(){
+        console.log('Read notes');
+    }
+})
+yargs.parse()
